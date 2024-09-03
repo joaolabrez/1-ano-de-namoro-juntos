d2b18033d665f7d05c4d08cc5523488bc4fcd91b
joaolabrez <gabrielsomewood@gmail.com>
2024-09-03 15:02:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showMessage() {
-    const message = "Eu te amo muito! ❤️";
+    const message = "Eu te amo pra sempre Sabrina Tormes! ❤️";
     const surpriseMessage = document.getElementById('surpriseMessage');
     surpriseMessage.textContent = message;
     surpriseMessage.classList.remove('hidden');
