9e4f8101886c8f398c8f663c9ba19b134eed3a65
joaolabrez <gabrielsomewood@gmail.com>
2024-09-03 14:18:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    const message = "Eu te amo muito! ❤️";
+    const surpriseMessage = document.getElementById('surpriseMessage');
+    surpriseMessage.textContent = message;
+    surpriseMessage.classList.remove('hidden');
+}
